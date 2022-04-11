23fc6c78545af7114a5279238808328e1478ab9d
Matias Mäkipelto <matias.timoaleksi@hotmail.com>
2022-04-11 16:03:42 +0300

    Improved environment Modified wind walkway Improved player collisions

--- Assets/_OwnAssets/Scripts/PlayerHitbox.cs
+++ Assets/_OwnAssets/Scripts/PlayerHitbox.cs
@@ -4,21 +4,17 @@ using UnityEngine;
 
 public class PlayerHitbox : MonoBehaviour
 {
-    private BoxCollider boxCollider;
-    private SphereCollider sphereCollider;
+    public GameObject customCollider;
     private Transform cam; // camera
     // Start is called before the first frame update
     void Start()
     {
-        boxCollider = GetComponent<BoxCollider>();
-        sphereCollider = GetComponent<SphereCollider>();
         cam = transform.GetChild(0).GetChild(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        boxCollider.center = new Vector3(cam.localPosition.x, boxCollider.center.y, cam.localPosition.z);
-        sphereCollider.center = new Vector3(cam.localPosition.x, sphereCollider.center.y, cam.localPosition.z);
+        customCollider.transform.localPosition = new Vector3(cam.localPosition.x, customCollider.transform.localPosition.y, cam.localPosition.z);
     }
 }
