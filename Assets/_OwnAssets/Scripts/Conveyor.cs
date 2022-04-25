47ee72915a787aa8d45b6c804160894e6e5124b7
Matias Mäkipelto <matias.timoaleksi@hotmail.com>
2022-04-25 15:48:43 +0300

    Added tilted walkways

--- Assets/_OwnAssets/Scripts/Conveyor.cs
+++ Assets/_OwnAssets/Scripts/Conveyor.cs
@@ -6,6 +6,7 @@ public class Conveyor : MonoBehaviour
 {
     public float speed;
     public float textureSpeedMultiplier;
+    public bool reverse;
 
     private Rigidbody rb;
     private Vector3 movement;
@@ -22,15 +23,17 @@ public class Conveyor : MonoBehaviour
     private void FixedUpdate()
     {
         // Scroll texture
-        texturePosition = texturePosition + speed * textureSpeedMultiplier;
+        texturePosition = texturePosition + speed * textureSpeedMultiplier * (reverse ? -1 : 1);
         if (texturePosition > 1)
             texturePosition--;
+        else if (texturePosition < 0)
+            texturePosition++;
         walkwayMaterial.mainTextureOffset = new Vector2(0, texturePosition);
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        movement = transform.forward * speed;
+        movement = transform.forward * speed * (reverse ? -1 : 1);
 
         rb.position -= movement;
         rb.MovePosition(rb.position + movement);
